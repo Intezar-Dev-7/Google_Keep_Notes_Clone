ce3a5361fca8beab95d35f16973255454966f112
Intezar-Dev-7 <Intezarcodes302@gmail.com>
2025-01-16 12:50:11 +0530

    made some changes in the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,6 +7,7 @@ import 'package:google_keep_notes_clone/services/firestore_db.dart';
 import 'package:google_keep_notes_clone/services/login_info.dart';
 import 'package:google_keep_notes_clone/services/auth.dart';
 import 'package:google_keep_notes_clone/utils/colors.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -21,95 +22,100 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Welcome to Notes App",
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        backgroundColor: cardColor,
-      ),
-      backgroundColor: cardColor,
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // App Logo
-              const CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/icon/appIconNotes.png'),
-              ),
-              const SizedBox(height: 20),
-
-              const Text(
-                "Your Notes, Anywhere!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "Sign in to sync and manage your notes seamlessly.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white70,
-                ),
-              ),
-
-              const SizedBox(height: 40),
-
-              // Google Sign-In Button
-              SignInButton(
-                Buttons.Google,
-                text: "Sign in with Google",
-                onPressed: () async {
-                  try {
-                    await signInWithGoogle();
-                    final User? currentUser = _auth.currentUser;
-
-                    if (currentUser != null) {
-                      LocalDataSaver.saveLoginData(true);
-                      LocalDataSaver.saveImg(currentUser.photoURL.toString());
-                      LocalDataSaver.saveMail(currentUser.email.toString());
-                      LocalDataSaver.saveName(
-                          currentUser.displayName.toString());
-                      LocalDataSaver.saveSyncSettings(false);
-
-                      await FireDB().getAllStoredNotes();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    }
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("An error occurred: $e")),
-                    );
-                  }
-                },
-              ),
-              const SizedBox(height: 20),
-              // Footer Text
-              const Text(
-                "We never share your data with anyone.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.white54,
-                ),
-              ),
-            ],
+        appBar: AppBar(
+          title: const Text(
+            "Welcome to Notes App",
+            style: TextStyle(color: Colors.white),
           ),
+          centerTitle: true,
+          backgroundColor: cardColor,
         ),
-      ),
-    );
+        backgroundColor: cardColor,
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  // App Logo
+                  const CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/icon/appIconNotes.png'),
+                  ),
+                  const SizedBox(height: 20),
+
+                  const Text(
+                    "Your Notes, Anywhere!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Sign in to sync and manage your notes seamlessly.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70,
+                    ),
+                  ),
+
+                  const SizedBox(height: 40),
+
+                  // Google Sign-In Button
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.white),
+                    width: 157,
+                    height: 35,
+                    child: TextButton(
+                      child: const Icon(Iconsax.google_1),
+                      onPressed: () async {
+                        try {
+                          await signInWithGoogle();
+                          final User? currentUser = _auth.currentUser;
+
+                          if (currentUser != null) {
+                            LocalDataSaver.saveLoginData(true);
+                            LocalDataSaver.saveImg(
+                                currentUser.photoURL.toString());
+                            LocalDataSaver.saveMail(
+                                currentUser.email.toString());
+                            LocalDataSaver.saveName(
+                                currentUser.displayName.toString());
+                            LocalDataSaver.saveSyncSettings(false);
+
+                            await FireDB().getAllStoredNotes();
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeScreen()),
+                            );
+                          }
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("An error occurred: $e")),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+                  // Footer Text
+                  const Text(
+                    "We never share your data with anyone.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white54,
+                    ),
+                  ),
+                ]))));
   }
 }
